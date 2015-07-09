b1ed7d14f128e810dee03c23cfb6b9f694989794
Adrien RAFFIN <raffinadrien@gmail.com>
2015-07-09 23:11:05 +0200

    Add log for current request

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
-package main 
+package main
 
 import (
-	"net/http"
 	"flag"
+	"log"
+	"net/http"
 )
 
 func main() {
 	var host = flag.String("host", ":8000", "Host adresse to use for static web server")
 	flag.Parse()
-	http.ListenAndServe(*host, http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("client=", r.RemoteAddr, " URL=", r.URL.Path)
+		http.FileServer(http.Dir(".")).ServeHTTP(w, r)
+	})
+	http.ListenAndServe(*host, nil)
 }
