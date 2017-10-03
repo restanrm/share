d42e3e1cd1682f7ef78e22d83259cdcbd77b63e5
Adrien RAFFIN <adrien.raffin@sekoia.fr>
2017-10-03 10:00:27 +0200

    feat: add headers and parameters information when verbose is set

--- main.go
+++ main.go
@@ -8,10 +8,23 @@ import (
 
 func main() {
 	var host = flag.String("host", ":8000", "Host adresse to use for static web server")
+	var verbose = flag.Bool("v", false, "set verbosity")
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Print("client=", r.RemoteAddr, " URL=", r.URL.Path)
+		log.Print("client=", r.RemoteAddr, " Method=", r.Method, " URL=", r.URL.Path)
+		if *verbose {
+			r.ParseForm()
+			log.Print("List of headers: ")
+			for header, value := range r.Header {
+				log.Print("\t", header, ": ", value)
+			}
+			log.Print("List parameters found in the request: ")
+			for form, value := range r.Form {
+				log.Print("\t", form, ": ", value)
+			}
+			log.Print()
+		}
 		http.FileServer(http.Dir(".")).ServeHTTP(w, r)
 	})
-	http.ListenAndServe(*host, nil)
+	log.Fatal(http.ListenAndServe(*host, nil))
 }
