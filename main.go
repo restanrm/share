50ddc8d37b346535a472cbc6d26f4590a81d9cf6
Adrien RAFFIN <adrien.raffin@sekoia.fr>
2017-10-04 11:57:55 +0200

    feat: Add livereload support

--- main.go
+++ main.go
@@ -5,12 +5,63 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/jaschaephraim/lrserver"
 )
+
+// Livereload
+// Include the following code to have livereload functionnality in you webpage:
+// <script>
+//   document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+//   ':35729/livereload.js?snipver=1"></' + 'script>')
+// </script>
+
+func startLiveReload() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer watcher.Close()
+
+	// get current path
+	ex, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Listening on path: ", ex)
+	err = watcher.Add(ex)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	lr := lrserver.New(lrserver.DefaultName, lrserver.DefaultPort)
+	go lr.ListenAndServe()
+
+	for {
+		select {
+		case event := <-watcher.Events:
+			lr.Reload(event.Name)
+		case err := <-watcher.Errors:
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
 
 func main() {
 	var host = flag.String("host", ":8000", "Host adresse to use for static web server")
 	var verbose = flag.Bool("v", false, "set verbosity")
+	var liveReload = flag.Bool("lr", false, "Enable livereload functionnality")
 	flag.Parse()
+
+	if *liveReload {
+		go startLiveReload()
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("client=", r.RemoteAddr, " Method=", r.Method, " URL=", r.URL.Path)
 		if *verbose {
